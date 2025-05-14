5460174a08b4b44bfe8ca85406f5caea9b137bd2
kumarSujal24 <kumarsujal186cse@gmail.com>
2025-05-14 21:43:09 +0530

    render version2

--- src/test/java/in/sam/main/StudentMangementSystemApplicationTests.java
+++ src/test/java/in/sam/main/StudentMangementSystemApplicationTests.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StudentMangementSystemApplicationTests {
+class StudentMangementSystemApplicationTests {}
 
-	@Test
-	void contextLoads() {
-	}
 
-}
